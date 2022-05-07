7c5fdc2c874e4059a7a2aa46e623e6474872ce7c
Tyler Dansby <tdansby7@gmail.com>
2022-05-07 15:14:57 -0500

    tubefeeding calculator

--- src/components/Body/Calculators/Calculator/CalculatorComponents/Components/TubeFeed.js
+++ src/components/Body/Calculators/Calculator/CalculatorComponents/Components/TubeFeed.js
@@ -5,6 +5,10 @@ import Formulas from './TubeFeedFormulas';
 function TubeFeed(){
     const [chosenFormula,setChosenFormula] = useState('');
     const [continuousRate,setContinuousRate] = useState(50);
+    const [kcalProvided, setKcalProvided] = useState(0);
+    const [proteinProvided, setProteinProvided] = useState(0);
+    const [freeWater, setFreeWater] = useState(0);
+    const [hrsDay,setHrsDay] = useState(0);
     
     const handleFormulaChange = (event) => {
         setChosenFormula(event.target.value);
@@ -12,7 +16,21 @@ function TubeFeed(){
     const handleContinuousRate = (event) => {
         setContinuousRate(Number(event.target.value));
     }
+    const handleHrsDay = (event) => {
+        if(Number(event.target.value)>0 && Number(event.target.value)<25){
+            setHrsDay(Number(event.target.value));
+        } 
+    }
 
+    useEffect(()=>{
+        const formulaToUse = Formulas[chosenFormula];
+        if(!formulaToUse){
+            return;
+        }
+        setKcalProvided(Math.round(formulaToUse.kcal/1000 * continuousRate * hrsDay));
+        setProteinProvided(Math.round(formulaToUse.protein/1000 * continuousRate * hrsDay));
+        setFreeWater(Math.round(formulaToUse.water/1000 * continuousRate * hrsDay));
+    },[chosenFormula,continuousRate,hrsDay])
     return(
         <div>
             <FormControl fullWidth>
@@ -37,8 +55,17 @@ function TubeFeed(){
                 step={1}
                 valueLabelDisplay="auto"
             ></Slider>
-            <Typography>{continuousRate}</Typography>
-            
+            <Typography>{continuousRate} ml/hr</Typography>
+            <InputLabel id="continuous-hrs-label">hrs/day</InputLabel>
+            <TextField 
+                type="number" 
+                labelId="continuous-hrs-label"
+                value={hrsDay}
+                onChange={handleHrsDay}
+            ></TextField>
+            <Typography>{kcalProvided} KCal</Typography>
+            <Typography>{proteinProvided}g Protein</Typography>
+            <Typography>{freeWater}ml Free Water</Typography>
         </div>
     )
 }
