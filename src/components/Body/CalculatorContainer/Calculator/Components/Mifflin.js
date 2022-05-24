540ca740aa45fd9f57d560b20a8dc19c97ec665b
Tyler Dansby <tdansby7@gmail.com>
2022-05-24 18:18:31 -0500

    mifflin and penn state

--- src/components/Body/CalculatorContainer/Calculator/Components/Mifflin.js
+++ src/components/Body/CalculatorContainer/Calculator/Components/Mifflin.js
@@ -1,7 +1,164 @@
+import { Typography, RadioGroup, FormControlLabel, Radio, TextField, Select, MenuItem } from '@mui/material';
+import {useState, useEffect} from 'react';
 function Mifflin(){
+    const [gender,setGender] = useState('');
+    const [weight,setWeight] = useState(0);
+    const [weightUnit,setWeightUnit] = useState('Lbs');
+    const [heightFeet,setHeightFeet] = useState(0);
+    const [heightInches,setHeightInches] = useState(0);
+    const [age,setAge] = useState(0);
+    const [output,setOutput] = useState('');
+    const [activityFactor,setActivityFactor] = useState(1);
+
+    useEffect(()=>{
+        let mifflinOutput='';
+        const heightInCm = ((heightFeet*12) + heightInches)*2.54;
+        const weightInKg = weightUnit==='Lbs' ? weight/2.205 : weight;
+        console.log(weightInKg);
+        if(!gender){
+            setOutput('Select Gender');
+            return;
+        }
+        if(gender==='male'){
+            mifflinOutput = (10*weightInKg) + (6.25*heightInCm) - (5*age) + 5;
+        }
+        else if(gender==='female'){
+            mifflinOutput = (10*weightInKg) + (6.25*heightInCm) - (5*age) + - 161;
+        }
+        mifflinOutput *= activityFactor;
+        setOutput(Math.floor(mifflinOutput) + ' kcal');
+    },[gender,weight,weightUnit,heightFeet,heightInches,age,activityFactor])
+
+    const handleActivityFactor = (event) => {
+        if(event.target.value>2){
+            setActivityFactor(2);
+        }
+        else if(event.target.value<1){
+            setActivityFactor(1)
+        }
+        else setActivityFactor(event.target.value);
+    }
+   const handleGender = (event) => {
+       setGender(event.target.value);
+   }
+   const handleFeet = (event) => {
+        if(event.target.value<0){
+            setHeightFeet(0);
+        }
+        else if(event.target.value>8){
+            setHeightFeet(8);
+        }
+        else setHeightFeet(Number(event.target.value));
+    }
+    const handleInches = (event) => {
+        if(event.target.value<0){
+            setHeightInches(0);
+        }
+        else if(event.target.value>11){
+            setHeightInches(11);
+        }
+        else setHeightInches(Number(event.target.value));
+    }
+    const handleWeightUnit = (event) => {
+        setWeightUnit(event.target.value);
+    }
+    const handleWeight = (event) => {
+        if(event.target.value<0){
+            setWeight(0);
+        }
+        else setWeight(Number(event.target.value));
+    }
+    const handleAge = (event) =>{
+        if(event.target.value<0){
+            setAge(0);
+        }
+        else if(event.target.value>123){
+            setAge(123);
+        }
+        else setAge(Number(event.target.value));
+    }
     return(
         <div>
             Mifflin
+            <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                id="gender-select"
+                labelId="gender-select"
+                value={gender}
+                onChange={handleGender}
+                sx={{flexDirection:'row'}}
+            >
+                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                <FormControlLabel value="male" control={<Radio />} label="Male" />
+            </RadioGroup>
+            <div id="heightContainer">
+                <Typography variant="p">Height</Typography>
+                <TextField 
+                    label="Feet"
+                    type="number"
+                    size="small"
+                    value={heightFeet}
+                    onChange={handleFeet}
+                    sx={{width:'100px'}}
+                >
+                </TextField>
+                <TextField
+                    label="Inches"
+                    type="number"
+                    size="small"
+                    value={heightInches}
+                    onChange={handleInches}
+                    sx={{width:'100px'}}
+                >
+                </TextField>
+            </div>
+            <div className="weightContainer">
+                <Typography variant="p">Current Weight</Typography>
+                <TextField
+                    label={weightUnit}
+                    type="number"
+                    size="small"
+                    value={weight}
+                    onChange={handleWeight}
+                    sx={{width:'100px'}}
+                >
+                </TextField>
+                <Select
+                    labelId="weightUnitInputLabel"
+                    id="weightUnitInput"
+                    value={weightUnit}
+                    label="Weight Unit"
+                    onChange={handleWeightUnit}
+                    size="small"
+                >
+                    <MenuItem value={'Lbs'}>Lbs</MenuItem>
+                    <MenuItem value={'Kg'}>Kg</MenuItem>
+                </Select>
+            </div>
+            <div className="ageContainer">
+                <Typography>Age</Typography>
+                <TextField
+                    type='number'
+                    size="small"
+                    onChange={handleAge}
+                    sx={{width:'100px'}}
+                    value={age}
+                >
+
+                </TextField>
+            </div>
+            <div>
+                <Typography>Activity Factor</Typography>
+                <TextField
+                    type="number"
+                    size='small'
+                    onChange={handleActivityFactor}
+                    sx={{width:'100px'}}
+                    value={activityFactor}
+                ></TextField>
+            </div>
+            <Typography variant="p">{output}</Typography>
         </div>
     )
 }
--- src/components/Body/CalculatorContainer/Calculator/Components/PennState.js
+++ src/components/Body/CalculatorContainer/Calculator/Components/PennState.js
@@ -1,7 +1,221 @@
+import { Typography, RadioGroup, FormControlLabel, Radio, TextField, Select, MenuItem } from '@mui/material';
+import {useState, useEffect} from 'react';
+//currently using 2003 penn state, need to add modified
+
 function PennState(){
+    const [gender,setGender] = useState('');
+    const [weight,setWeight] = useState(0);
+    const [weightUnit,setWeightUnit] = useState('Lbs');
+    const [heightFeet,setHeightFeet] = useState(0);
+    const [heightInches,setHeightInches] = useState(0);
+    const [age,setAge] = useState(0);
+    const [activityFactor,setActivityFactor] = useState(1);
+    const [penn,setPenn] = useState('');
+    const [modifiedPenn,setModifiedPenn] = useState('');
+    const [tMax,setTmax] = useState(0);
+    const [tMaxUnit,setTMaxUnit] = useState('Celsius');
+    const [ve,setVe] = useState(0);
+
+    useEffect(()=>{
+        let mifflinOutput='';
+        let pennOutput='';
+        let modifiedPennOutput='';
+        const heightInCm = ((heightFeet*12) + heightInches)*2.54;
+        const weightInKg = weightUnit==='Lbs' ? weight/2.205 : weight;
+        console.log(weightInKg);
+        if(!gender){
+            setPenn('Select Gender');
+            return;
+        }
+        if(gender==='male'){
+            mifflinOutput = (10*weightInKg) + (6.25*heightInCm) - (5*age) + 5;
+        }
+        else if(gender==='female'){
+            mifflinOutput = (10*weightInKg) + (6.25*heightInCm) - (5*age) + - 161;
+        }
+        mifflinOutput *= activityFactor;
+        pennOutput = mifflinOutput*0.96 + tMax*167 + ve*31 - 6212;
+        setPenn(pennOutput);
+    },[gender,weight,weightUnit,heightFeet,heightInches,age,activityFactor,tMax,tMaxUnit,ve])
+
+    const handleTmaxUnit = (event) => {
+        setTMaxUnit(event.target.value);
+    }
+    const handleTmax = (event) => {
+        if(event.target.value<0){
+            setTmax(0);
+        }
+        else if(event.target.value>120){
+            setTmax(120);
+        }
+        else setTmax(event.target.value);
+    }
+    const handleVe = (event) =>{
+        if(event.target.value<0){
+            setVe(0);
+        }
+        else if(event.target.value>100){
+            setVe(100);
+        }
+        else setVe(event.target.value);
+    }
+    const handleActivityFactor = (event) => {
+        if(event.target.value>2){
+            setActivityFactor(2);
+        }
+        else if(event.target.value<1){
+            setActivityFactor(1)
+        }
+        else setActivityFactor(event.target.value);
+    }
+   const handleGender = (event) => {
+       setGender(event.target.value);
+   }
+   const handleFeet = (event) => {
+        if(event.target.value<0){
+            setHeightFeet(0);
+        }
+        else if(event.target.value>8){
+            setHeightFeet(8);
+        }
+        else setHeightFeet(Number(event.target.value));
+    }
+    const handleInches = (event) => {
+        if(event.target.value<0){
+            setHeightInches(0);
+        }
+        else if(event.target.value>11){
+            setHeightInches(11);
+        }
+        else setHeightInches(Number(event.target.value));
+    }
+    const handleWeightUnit = (event) => {
+        setWeightUnit(event.target.value);
+    }
+    const handleWeight = (event) => {
+        if(event.target.value<0){
+            setWeight(0);
+        }
+        else setWeight(Number(event.target.value));
+    }
+    const handleAge = (event) =>{
+        if(event.target.value<0){
+            setAge(0);
+        }
+        else if(event.target.value>123){
+            setAge(123);
+        }
+        else setAge(Number(event.target.value));
+    }
     return(
         <div>
-            Penn
+            Mifflin
+            <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                id="gender-select"
+                labelId="gender-select"
+                value={gender}
+                onChange={handleGender}
+                sx={{flexDirection:'row'}}
+            >
+                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                <FormControlLabel value="male" control={<Radio />} label="Male" />
+            </RadioGroup>
+            <div id="heightContainer">
+                <Typography variant="p">Height</Typography>
+                <TextField 
+                    label="Feet"
+                    type="number"
+                    size="small"
+                    value={heightFeet}
+                    onChange={handleFeet}
+                    sx={{width:'100px'}}
+                >
+                </TextField>
+                <TextField
+                    label="Inches"
+                    type="number"
+                    size="small"
+                    value={heightInches}
+                    onChange={handleInches}
+                    sx={{width:'100px'}}
+                >
+                </TextField>
+            </div>
+            <div className="weightContainer">
+                <Typography variant="p">Current Weight</Typography>
+                <TextField
+                    label={weightUnit}
+                    type="number"
+                    size="small"
+                    value={weight}
+                    onChange={handleWeight}
+                    sx={{width:'100px'}}
+                >
+                </TextField>
+                <Select
+                    labelId="weightUnitInputLabel"
+                    id="weightUnitInput"
+                    value={weightUnit}
+                    label="Weight Unit"
+                    onChange={handleWeightUnit}
+                    size="small"
+                >
+                    <MenuItem value={'Lbs'}>Lbs</MenuItem>
+                    <MenuItem value={'Kg'}>Kg</MenuItem>
+                </Select>
+            </div>
+            <div className="ageContainer">
+                <Typography>Age</Typography>
+                <TextField
+                    type='number'
+                    size="small"
+                    onChange={handleAge}
+                    sx={{width:'100px'}}
+                    value={age}
+                >
+
+                </TextField>
+            </div>
+            <div>
+                <Typography>Activity Factor</Typography>
+                <TextField
+                    type="number"
+                    size='small'
+                    onChange={handleActivityFactor}
+                    sx={{width:'100px'}}
+                    value={activityFactor}
+                ></TextField>
+            </div>
+            <TextField
+                    type="number"
+                    size='small'
+                    onChange={handleTmax}
+                    sx={{width:'100px'}}
+                    value={tMax}
+                    label='TMax'
+            ></TextField>
+            <Select
+                    labelId="tMaxUnitInputLabel"
+                    id="tMaxUnitInput"
+                    value={tMaxUnit}
+                    onChange={handleTmaxUnit}
+                    size="small"
+                >
+                    <MenuItem value={'Celsius'}>Celsius</MenuItem>
+                    <MenuItem value={'Fahrenheit'}>Fahrenheight</MenuItem>
+            </Select>
+            <TextField
+                    type="number"
+                    size='small'
+                    onChange={handleVe}
+                    sx={{width:'100px'}}
+                    value={ve}
+                    label='VE in L/min'
+            ></TextField>
+            <Typography variant="p">{penn}</Typography>
+            <Typography variant="p">{modifiedPenn}</Typography>
         </div>
     )
 }
