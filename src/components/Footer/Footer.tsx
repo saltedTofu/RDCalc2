f3c0d6402057bc104856222e68d4b15001080b90
Tyler Dansby <tdansby7@gmail.com>
2023-05-29 11:08:37 -0700

    began ts migration

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,12 @@
 import {Typography, Box, Link} from '@mui/material';
-import {Link as RouterLink} from 'react-router-dom';
 
 function Footer() {
 
     return(
         <Box sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',textAlign:'center', width:'85%',padding:'10px',borderTop:'3px solid rgba(0,0,0,0.2)'}}>
-            <Typography variant="p">Find any errors? Let me know and I'll fix them!</Typography>
-            <Link variant="p" href="mailto:saltedTofuDev@gmail.com">saltedTofuDev@gmail.com</Link>
-            <Typography variant="p">version 1.0.0.1</Typography> 
+            <Typography>Find any errors? Let me know and I'll fix them!</Typography>
+            <Link href="mailto:saltedTofuDev@gmail.com">saltedTofuDev@gmail.com</Link>
+            <Typography >version 1.0.0.1</Typography> 
         </Box>       
     )
 }
